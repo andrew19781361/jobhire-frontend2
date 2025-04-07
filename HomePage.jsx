eed362a935c6b1ba8794fc37e170b554a29598d6
Andrew Smiley <75382539+andrew19781361@users.noreply.github.com>
2025-04-06 17:22:39 -0700

    Add files via upload

---
+++ HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to JobHire!</h1>
+      <p>This is your home page. Start building your app here.</p>
+    </div>
+  );
+};
+
+export default HomePage;
